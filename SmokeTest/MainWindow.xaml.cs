93d2a523e786252557c52e4078d3029a3fdaea91
RELIAS\vhoehn <VHoehn@reliaslearning.com>
2018-10-01 07:27:06 -0400

    Fixed issue with problem with deployment.  Error are tryed out.  So it can open.

--- SmokeTest/MainWindow.xaml.cs
+++ SmokeTest/MainWindow.xaml.cs
@@ -103,21 +103,37 @@ namespace SmokeTest
             InitializeComponent();
             DataContext = this;
             WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
-            //Assembly ass = Assembly.GetExecutingAssembly();
-            //FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(ass.Location);
-            //var ver = Assembly.GetExecutingAssembly().GetName().Version;
-            //var strVer = fvi.ProductVersion;
-            //var strVersion = string.Format("{0}.{1}.{2}.{3}",ver.Major,ver.Minor,ver.Build,ver.Revision);
-            ste = new SmokeTestsEntities();
-            Title = string.Format("Smoking ({0})", CurrentVersion);
-            Releases = GetReleases();
-            TheRelease = GetCurrentRelease();
             //PopulateReportSummaries();
+            try
+            {
+                Assembly ass = Assembly.GetExecutingAssembly();
+                FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(ass.Location);
+                var ver = Assembly.GetExecutingAssembly().GetName().Version;
+                var strVer = fvi.ProductVersion;
+                var strVersion = string.Format("{0}.{1}.{2}.{3}", ver.Major, ver.Minor, ver.Build, ver.Revision);
+
+                ste = new SmokeTestsEntities();
+                Title = string.Format("Smoking ({0})", CurrentVersion);
+                Releases = GetReleases();
+                TheRelease = GetCurrentRelease();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("MainWindow Error: {0}", ex.Message));
+            }
         }
 
         private Release GetCurrentRelease()
         {
-            Release rel = Releases.First();
+            Release rel = null;
+            try
+            {
+                rel = Releases.First();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("GetCurrentRelease Error: {0}", ex.Message));
+            }
             return rel;
         }
 
@@ -129,117 +145,124 @@ namespace SmokeTest
 
         private void PopulateReportSummaries()
         {
-            Cursor = Cursors.Wait;
-            var dateString = "2018, 9, 26";
-            var goodReport = "";
-            var badReport = "";
-            var uglyReport = "";
-            var untestedReport = "";
-            var goodSection = "";
-            var badSection = "";
-            var uglySection = "";
-            var untestedSection = "";
-
-            List<Status> lstStatuses = ste.Status.ToList();
-            if (TheRelease != null)
+            try
             {
-                if (TheRelease.Date != null)
+                Cursor = Cursors.Wait;
+                var dateString = "2018, 9, 26";
+                var goodReport = "";
+                var badReport = "";
+                var uglyReport = "";
+                var untestedReport = "";
+                var goodSection = "";
+                var badSection = "";
+                var uglySection = "";
+                var untestedSection = "";
+
+                List<Status> lstStatuses = ste.Status.ToList();
+                if (TheRelease != null)
                 {
-                    dateString = TheRelease.Date.Value.Year.ToString() + ", " + TheRelease.Date.Value.Month.ToString() + ", " + TheRelease.Date.Value.Day.ToString();
+                    if (TheRelease.Date != null)
+                    {
+                        dateString = TheRelease.Date.Value.Year.ToString() + ", " + TheRelease.Date.Value.Month.ToString() + ", " + TheRelease.Date.Value.Day.ToString();
+                    }
+
+                    var rs = new List<ReportSummmary>();
+                    foreach (Status s in lstStatuses)
+                    {
+                        //new ReportSummmary() { Status = "Good", Count = 20 },
+                        var count = 0;
+                        try
+                        {
+                            var someStuff = ste.Report_Evaluations.Where(a => a.Release_ID == TheRelease.Id & a.Status_ID == s.Id).ToList();
+                            count = someStuff.Count();
+                        }
+                        catch (Exception ex) { MessageBox.Show(ex.ToString()); }
+                        rs.Add(new ReportSummmary() { Status = s.Name, Count = count });
+
+                        if (s.Id == 1)
+                        {
+                            untestedReport = count.ToString();
+                        }
+                        else if (s.Id == 2)
+                        {
+                            goodReport = count.ToString();
+                        }
+                        else if (s.Id == 3)
+                        {
+                            badReport = count.ToString();
+                        }
+                        else if (s.Id == 4)
+                        {
+                            uglyReport = count.ToString();
+                        }
+                    };
+                    var ss = new List<ReportSummmary>();
+                    foreach (Status s in lstStatuses)
+                    {
+                        var count = 0;
+                        //new ReportSummmary() { Status = "Good", Count = 20 },
+                        var someStuff = ste.Section_Evaluations.Where(a => a.Release_ID == TheRelease.Id & a.Status_ID == s.Id).ToList();
+                        count = someStuff.Count();
+                        ss.Add(new ReportSummmary() { Status = s.Name, Count = count });
+
+                        if (s.Id == 1)
+                        {
+                            untestedSection = count.ToString();
+                        }
+                        else if (s.Id == 2)
+                        {
+                            goodSection = count.ToString();
+                        }
+                        else if (s.Id == 3)
+                        {
+                            badSection = count.ToString();
+                        }
+                        else if (s.Id == 4)
+                        {
+                            uglySection = count.ToString();
+                        }
+                    };
+                    ReportSummmaries = rs;
+                    SectionSummmaries = ss;
                 }
 
-                var rs = new List<ReportSummmary>();
-                foreach (Status s in lstStatuses)
-                {
-                    //new ReportSummmary() { Status = "Good", Count = 20 },
-                    var count = 0;
-                    try
-                    {
-                        var someStuff = ste.Report_Evaluations.Where(a => a.Release_ID == TheRelease.Id & a.Status_ID == s.Id).ToList();
-                        count = someStuff.Count();
-                    }
-                    catch (Exception ex) { MessageBox.Show(ex.ToString()); }
-                    rs.Add(new ReportSummmary() { Status = s.Name, Count = count });
+                string appDir = Environment.CurrentDirectory;
+                int post = appDir.LastIndexOf("bin\\Debug");
+                string strDir = appDir.Remove(post);
+                string templatePageLink = strDir + "StatusTemplate.html";
+                string reportStatusPage = strDir + "ReportStatus.html";
+                string sectionStatusPage = strDir + "SectionStatus.html";
+                string pageSource = System.IO.File.ReadAllText(templatePageLink);
+                string reportSource = "";
+                string sectionSource = "";
 
-                    if (s.Id == 1)
-                    {
-                        untestedReport = count.ToString();
-                    }
-                    else if (s.Id == 2)
-                    {
-                        goodReport = count.ToString();
-                    }
-                    else if (s.Id == 3)
-                    {
-                        badReport = count.ToString();
-                    }
-                    else if (s.Id == 4)
-                    {
-                        uglyReport = count.ToString();
-                    }
-                };
-                var ss = new List<ReportSummmary>();
-                foreach (Status s in lstStatuses)
-                {
-                    var count = 0;
-                    //new ReportSummmary() { Status = "Good", Count = 20 },
-                    var someStuff = ste.Section_Evaluations.Where(a => a.Release_ID == TheRelease.Id & a.Status_ID == s.Id).ToList();
-                    count = someStuff.Count();
-                    ss.Add(new ReportSummmary() { Status = s.Name, Count = count });
+                // Report Status
+                reportSource = pageSource.Replace("@Date", dateString);
+                reportSource = reportSource.Replace("@Good", goodReport);
+                reportSource = reportSource.Replace("@Bad", badReport);
+                reportSource = reportSource.Replace("@Ugly", uglyReport);
+                reportSource = reportSource.Replace("@Untested", untestedReport);
 
-                    if (s.Id == 1)
-                    {
-                        untestedSection = count.ToString();
-                    }
-                    else if (s.Id == 2)
-                    {
-                        goodSection = count.ToString();
-                    }
-                    else if (s.Id == 3)
-                    {
-                        badSection = count.ToString();
-                    }
-                    else if (s.Id == 4)
-                    {
-                        uglySection = count.ToString();
-                    }
-                };
-                ReportSummmaries = rs;
-                SectionSummmaries = ss;
+                // Section Status
+                sectionSource = pageSource.Replace("@Date", dateString);
+                sectionSource = sectionSource.Replace("@Good", goodSection);
+                sectionSource = sectionSource.Replace("@Bad", badSection);
+                sectionSource = sectionSource.Replace("@Ugly", uglySection);
+                sectionSource = sectionSource.Replace("@Untested", untestedSection);
+
+                System.IO.File.WriteAllText(reportStatusPage, reportSource);
+                System.IO.File.WriteAllText(sectionStatusPage, sectionSource);
+
+
+                Uri pageUriR = new Uri(reportStatusPage);
+                Uri pageUriS = new Uri(sectionStatusPage);
+                wbReports.Source = pageUriR;
+                wbSections.Source = pageUriS;
             }
-
-            string appDir = Environment.CurrentDirectory;
-            int post = appDir.LastIndexOf("bin\\Debug");
-            string strDir = appDir.Remove(post);
-            string templatePageLink = strDir + "StatusTemplate.html";
-            string reportStatusPage = strDir + "ReportStatus.html";
-            string sectionStatusPage = strDir + "SectionStatus.html";
-            string pageSource = System.IO.File.ReadAllText(templatePageLink);
-            string reportSource = "";
-            string sectionSource = "";
-
-            // Report Status
-            reportSource = pageSource.Replace("@Date", dateString);
-            reportSource = reportSource.Replace("@Good", goodReport);
-            reportSource = reportSource.Replace("@Bad", badReport);
-            reportSource = reportSource.Replace("@Ugly", uglyReport);
-            reportSource = reportSource.Replace("@Untested", untestedReport);
-
-            // Section Status
-            sectionSource = pageSource.Replace("@Date", dateString);
-            sectionSource = sectionSource.Replace("@Good", goodSection);
-            sectionSource = sectionSource.Replace("@Bad", badSection);
-            sectionSource = sectionSource.Replace("@Ugly", uglySection);
-            sectionSource = sectionSource.Replace("@Untested", untestedSection);
-
-            System.IO.File.WriteAllText(reportStatusPage, reportSource);
-            System.IO.File.WriteAllText(sectionStatusPage, sectionSource);
-
-
-            Uri pageUriR = new Uri(reportStatusPage);
-            Uri pageUriS = new Uri(sectionStatusPage);
-            wbReports.Source = pageUriR;
-            wbSections.Source = pageUriS;
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("PopulateReportSummaries Error:{0}/nInnerException:{1}/n{2}", ex.Message, ex.InnerException, ex.Source.ToArray(), ex.ToString()));
+            }
             Cursor = Cursors.Arrow;
         }
 
